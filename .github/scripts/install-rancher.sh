326dd267dde082233afffa0168fa6790b9dca040
Francesco Torchia <francesco.torchia@suse.com>
2024-07-22 10:41:25 +0200

    Login to Rancher and get Kubeconfig file
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- .github/scripts/install-rancher.sh
+++ .github/scripts/install-rancher.sh
@@ -7,4 +7,50 @@ VERSION=$1
 echo "Starting container ..."
 ID=$(docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged -e CATTLE_BOOTSTRAP_PASSWORD=admin rancher/rancher:${VERSION})
 
+if [ $? -ne 0 ]; then
+  echo "An error occurred running the Docker container"
+  exit 1
+fi
+
+echo ""
 echo "Container Id: ${ID}"
+
+echo ""
+echo "Waiting for backend to become ready..."
+TIME=0
+while [[ "$(curl --insecure -s -m 5 -o /dev/null -w ''%{http_code}'' https://localhost)" != "200" ]]; do
+  sleep 5;
+  TIME=$((TIME + 5))
+  printf "\r${TIME}s ... "
+done
+
+RANCHER_TOKEN=$(curl -vkL \
+  -H 'Content-Type: application/json' \
+  -d '{"description":"UI session","responseType":"cookie","username":"admin","password":"admin"}' \
+  -X POST \
+  https://localhost/v3-public/localProviders/local?action=login 2>&1 | \
+  awk -F'[=;]' '/Set-Cookie/{gsub(" ","",$2);print $2;exit}')
+
+echo ""
+echo "Get kubeconfig from local cluster ..."
+
+HEADER=$(echo "Cookie: R_SESS=$RANCHER_TOKEN")
+curl -kL \
+  -H 'Content-Type: application/json' \
+  -X POST \
+  -H "$HEADER" \
+  https://localhost/v3/clusters/local?action=generateKubeconfig | \
+  yq '.config' > kubeconfig.yaml
+
+export KUBECONFIG=kubeconfig.yaml
+
+sleep 2;
+
+echo ""
+kubectl cluster-info
+if [ $? -ne 0 ]; then
+  echo "Unable to get Rancher resources"
+  exit 1
+fi
+
+echo "Done"
