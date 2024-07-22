366abd0fa930f45f12f9c1f7806c7100413c4d1c
Francesco Torchia <francesco.torchia@suse.com>
2024-07-22 11:03:30 +0200

    Clean-up install-rancher.sh script
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- .github/scripts/install-rancher.sh
+++ .github/scripts/install-rancher.sh
@@ -2,10 +2,19 @@
 
 # Helper script to run Rancher in a Docker container
 
-VERSION=$1
+VERSION="master-head"
 
-echo "Starting container ..."
-ID=$(docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged -e CATTLE_BOOTSTRAP_PASSWORD=admin rancher/rancher:${VERSION})
+if [ -n "$1" ]; then
+  VERSION=$1
+fi
+
+IMAGE="rancher/rancher:${VERSION}"
+
+echo ""
+echo "Starting '${IMAGE}' container ..."
+
+echo ""
+ID=$(docker run -d --restart=unless-stopped -p 80:80 -p 443:443 --privileged -e CATTLE_BOOTSTRAP_PASSWORD=admin ${IMAGE})
 
 if [ $? -ne 0 ]; then
   echo "An error occurred running the Docker container"
@@ -16,7 +25,8 @@ echo ""
 echo "Container Id: ${ID}"
 
 echo ""
-echo "Waiting for backend to become ready..."
+echo "Waiting for backend to become ready"
+
 TIME=0
 while [[ "$(curl --insecure -s -m 5 -o /dev/null -w ''%{http_code}'' https://localhost)" != "200" ]]; do
   sleep 5;
@@ -24,17 +34,20 @@ while [[ "$(curl --insecure -s -m 5 -o /dev/null -w ''%{http_code}'' https://loc
   printf "\r${TIME}s ... "
 done
 
+echo ""
+echo "Login to Rancher"
+
 RANCHER_TOKEN=$(curl -vkL \
   -H 'Content-Type: application/json' \
-  -d '{"description":"UI session","responseType":"cookie","username":"admin","password":"admin"}' \
+  -d '{"username":"admin","password":"admin","responseType":"cookie"}' \
   -X POST \
   https://localhost/v3-public/localProviders/local?action=login 2>&1 | \
   awk -F'[=;]' '/Set-Cookie/{gsub(" ","",$2);print $2;exit}')
 
 echo ""
-echo "Get kubeconfig from local cluster ..."
+echo "Get kubeconfig from local cluster"
 
-HEADER=$(echo "Cookie: R_SESS=$RANCHER_TOKEN")
+HEADER="Cookie: R_SESS=$RANCHER_TOKEN"
 curl -kL \
   -H 'Content-Type: application/json' \
   -X POST \
