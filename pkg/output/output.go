9e346de34a2333e952709d9fb90e56dbe0ad0b28
Francesco Torchia <francesco.torchia@suse.com>
2024-07-30 11:32:23 +0200

    Clean up Output functions; Remove ManagedFields from project output
    Signed-off-by: Francesco Torchia <francesco.torchia@suse.com>

--- pkg/output/output.go
+++ pkg/output/output.go
@@ -8,26 +8,24 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-func Print[T any](ctx context.Context, items []T, formatter string, def func(item T) string) error {
-	for _, item := range items {
-		switch formatter {
-		case "yaml":
-			yamlData, err := yaml.Marshal(item)
-			if err != nil {
-				return err
-			}
-
-			fmt.Println(string(yamlData))
-		case "json":
-			jsonData, err := json.MarshalIndent(item, "", "  ")
-			if err != nil {
-				return err
-			}
-
-			fmt.Println(string(jsonData))
-		default:
-			fmt.Println(def(item))
+func Print[T any](ctx context.Context, item T, formatter string, def func(item T) string) error {
+	switch formatter {
+	case "yaml":
+		yamlData, err := yaml.Marshal(item)
+		if err != nil {
+			return err
 		}
+
+		fmt.Println(string(yamlData))
+	case "json":
+		jsonData, err := json.MarshalIndent(item, "", "  ")
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(string(jsonData))
+	default:
+		fmt.Println(def(item))
 	}
 
 	return nil
